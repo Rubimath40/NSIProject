43a197d5c0038e5671e09bf97446f75dd8e2af52
Rubimath40 <matcbt10@gmail.com>
2026-09-12 14:50:57 +0200

    [feat]: Created base_converter to convert a number into a different base, both specified by user.

---
+++ base_converter.py
@@ -0,0 +1,23 @@
+"""
+This script asks the user for two numbers: a and b; then it convert a into base b.
+It require both a and b are integer with a > 0 and  1 < b < 11.
+"""
+
+a = int(input("input your number: "))
+b = int(input("Convert %s to base: "%a))
+
+def divide(divisor, dividend):
+    quotient  = divisor // dividend
+    remainder = divisor %  dividend
+    return (quotient, remainder)
+
+def convert(number, base):
+    list_remainder = ""
+    quotient, remainder = divide(number, base)
+    list_remainder += str(remainder)
+    while quotient != 0:
+        quotient, remainder = divide(quotient, base)
+        list_remainder += str(remainder)
+    return list_remainder[::-1]
+
+print(convert(a, b))
